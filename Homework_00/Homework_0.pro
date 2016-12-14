bcbfb78a6998514ceef752a184a9d6de20263a89
Ethan Travis <etravis0206@gmail.com>
2016-12-13 20:08:49 -0500

    First Commit!!!

---
+++ Homework_00/Homework_0.pro
@@ -0,0 +1,12 @@
+QT += core
+QT -= gui
+
+CONFIG += c++11
+
+TARGET = Homework_0
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
---
+++ Homework_00/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        switch(i)
+      {
+        case 0:
+            cout << "Apple" << endl;
+            break;
+
+        case 1:
+            cout << "Orange" << endl;
+            break;
+
+        case 2:
+            cout << "Grape" << endl;
+            break;
+
+        case 3:
+            cout << "Pear" << endl;
+            break;
+
+        case 4:
+            cout << "Banana" << endl;
+            break;
+      }
+    }
+
+
+
+    return 0;
+}
